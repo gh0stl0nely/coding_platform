4d83c7d067f00c847b2136e0e2a1e80f09fb4d78
Demi Wu <demiwu96@gmail.com>
2020-07-05 00:30:49 -0400

    linear search visualization

--- client/src/pages/VisualPage.js
+++ client/src/pages/VisualPage.js
@@ -3,6 +3,7 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Chips from "../components/Chips";
 import Box from '@material-ui/core/Box';
+import LinearSearch from "../algorithms/LinearSearch";
 import Copyright from "../components/Copyright";
 
 function VisualPage() {
@@ -18,7 +19,7 @@ function VisualPage() {
             </Grid>
             <Grid container justify="center" alignItems="center">
             <Grid item xs={12} style={{ textAlign: "center", height: "300px", backgroundColor: "#dae1e7" }}>
-                    <p>Contents go here: explaination, videoes</p>
+                    <LinearSearch />
                 </Grid>
             </Grid>
             <Box mt={8}>
