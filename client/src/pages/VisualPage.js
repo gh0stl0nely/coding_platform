86ec832e38c65bede99063b105ad4fae1213fd6b
Demi Wu <demiwu96@gmail.com>
2020-07-07 22:46:13 -0400

    linear search visualization function

--- client/src/pages/VisualPage.js
+++ client/src/pages/VisualPage.js
@@ -8,28 +8,31 @@ import BinarySearch from "../algorithms/BinarySearch";
 import Copyright from "../components/Copyright";
 
 function VisualPage() {
+    const options = ["linear search", "binary search"]
+
+    function renderChips() {
+        return options.map(item => {
+            return <Chips name={item} />
+        })
+    }
+
     return (
-        <Container maxWidth="md" style={{ marginTop: "50px" }}>
+        <Container maxWidth="lg" style={{ marginTop: "50px" }}>
             <Grid container style={{ padding: "20px 0px" }} justify="center" alignItems="center">
-                <p>chips go here</p>
-                <Chips name="tag" />
-                <Chips name="tag" />
-                <Chips name="tag" />
-                <Chips name="tag" />
-                <Chips name="tag" />
+                {renderChips()}
             </Grid>
             <Grid container justify="center" alignItems="center">
-            <Grid item xs={12} style={{ textAlign: "center", height: "300px", backgroundColor: "#dae1e7" }}>
+                <Grid item xs={12} style={{ textAlign: "center", height: "300px", backgroundColor: "#dae1e7" }}>
                     <LinearSearch />
-            </Grid>
-            <Grid item xs={12} style={{ textAlign: "center", height: "300px", backgroundColor: "#dae1e7" }}>
+                </Grid>
+                <Grid item xs={12} style={{ textAlign: "center", height: "300px", backgroundColor: "#dae1e7" }}>
                     <BinarySearch />
+                </Grid>
             </Grid>
-            </Grid>
-            
+
             <Box mt={8}>
-          <Copyright />
-        </Box>
+                <Copyright />
+            </Box>
         </Container>
     )
 }
